8166675413f8b95f110869d7a4b87e785bfbe9d2
Rachid Ben Moussa <rachid.benmoussa@luminis.eu>
2020-10-11 20:19:52 +0200

    Added s3 bucket and CloudFront

--- lib/aws.blog-lambda-stack.ts
+++ lib/aws.blog-lambda-stack.ts
@@ -1,7 +1,10 @@
 import {AssetCode, Function, Runtime} from "@aws-cdk/aws-lambda"
-import {CfnOutput, Duration, Stack, StackProps} from '@aws-cdk/core';
+import {CfnOutput, Duration, RemovalPolicy, Stack, StackProps} from '@aws-cdk/core';
 import {Construct} from "@aws-cdk/core/lib/construct-compat";
 import {LambdaIntegration, RestApi} from "@aws-cdk/aws-apigateway"
+import cdk = require('@aws-cdk/core');
+import {Bucket} from "@aws-cdk/aws-s3";
+import cloudfront = require('@aws-cdk/aws-cloudfront');
 
 export class AwsBlogLambdaStack extends Stack {
   public readonly urlOutput: CfnOutput;
@@ -30,5 +33,54 @@ export class AwsBlogLambdaStack extends Stack {
 
     // Make the URL part of the outputs of CloudFormation (see the Outputs tab of this stack in the AWS Console)
     this.urlOutput = new CfnOutput(this, 'Url', { value: api.url, });
+
+
+    let bucketName
+    // Content bucket
+    const websiteBucket = new Bucket(this, 'SiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: true,
+
+      // The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+      // the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+      // DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
+    new CfnOutput(this, 'BUCKET_NAME', { value: websiteBucket.bucketName });
+    new cdk.CfnOutput(this, 'BUCKET_URL', {
+      description: 'The URL of the website',
+      value: websiteBucket.bucketWebsiteUrl
+    });
+
+
+    // CloudFront distribution that provides HTTPS
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+      // aliasConfiguration: {
+      //     acmCertRef: certificateArn,
+      //     names: [ siteDomain ],
+      //     sslMethod: cloudfront.SSLMethod.SNI,
+      //     securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_1_2016,
+      // },
+      // originConfigs: [
+      //   {
+      //     s3OriginSource: {
+      //       s3BucketSource: websiteBucket
+      //     },
+      //     behaviors : [ {isDefaultBehavior: true}]
+      //   }
+      // ],
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: websiteBucket
+          },
+          behaviors : [ {isDefaultBehavior: true}],
+        }
+      ]
+    });
+    new cdk.CfnOutput(this, 'Domain name', { value: distribution.distributionDomainName });
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+
   }
 }
